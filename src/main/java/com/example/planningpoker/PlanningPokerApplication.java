759ba778a7f136c7123da8f5f19d914249ccbee4
samitkumarpatel <samitkumarpatel@gmail.com>
2023-09-23 12:42:36 +0200

    html form to capture user data

--- src/main/java/com/example/planningpoker/PlanningPokerApplication.java
+++ src/main/java/com/example/planningpoker/PlanningPokerApplication.java
@@ -1,11 +1,16 @@
 package com.example.planningpoker;
 
+import com.example.planningpoker.repositories.RoomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class PlanningPokerApplication {
@@ -17,15 +22,15 @@ public class PlanningPokerApplication {
 }
 
 @Controller
+@RequiredArgsConstructor
 class PlanningPokerController {
-	@GetMapping("/{roomId}")
-	public String joiningForm(@PathVariable String roomId) {
-		return "joining-form";
-	}
+	final RoomRepository roomRepository;
 
-	@PostMapping("/{roomId}/join")
-	public String join() {
-		return "room";
+	@GetMapping("/{roomId}")
+	public Mono<String> joiningForm(@PathVariable String roomId) {
+		return roomRepository
+				.roomById(UUID.fromString(roomId))
+				.map(room -> "room");
 	}
 }
 
